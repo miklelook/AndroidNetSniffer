e6e063e31a6b55436187696b4d8da1bc90846c2f
weishuxin <weishuxin@maoyan.com>
2019-11-29 20:03:06 +0800

    v1.0.0 完成请求Header

--- sniffer-okhttp/src/main/java/com/wei/sniffer/okhttp/OkHttpSniffer.kt
+++ sniffer-okhttp/src/main/java/com/wei/sniffer/okhttp/OkHttpSniffer.kt
@@ -2,6 +2,7 @@ package com.wei.sniffer.okhttp
 
 import com.wei.sniffer.cache.*
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import okio.Buffer
 import okio.GzipSource
@@ -10,12 +11,74 @@ import java.nio.charset.StandardCharsets
 import java.util.concurrent.TimeUnit
 
 class OkHttpSniffer : Interceptor {
+    var startNs: Long? = null
     override fun intercept(chain: Interceptor.Chain): Response {
 
 
         // 处理请求
         val request = chain.request()
+        startNs = System.nanoTime()
+        val snifferLog = setRequestData(request)
+        val response = chain.proceed(request)
+        setResponseData(snifferLog, response)
+        return response
+    }
 
+    private fun setResponseData(snifferLog: SnifferLog, response: Response?) {
+
+        if (response != null) {
+            val responseBody = response.body!!
+            val headers = response.headers
+            snifferLog.response.contentLength = responseBody.contentLength()
+            snifferLog.response.code = response.code
+            snifferLog.statusCode = response.code.toString()
+            snifferLog.response.message = if (response.message.isEmpty()) "" else ' ' + response.message
+            snifferLog.response.duration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - (startNs
+                    ?: 0L))
+            snifferLog.response.scheme = snifferLog.url.protocol
+            snifferLog.response.headers = response.headers.toMultimap()
+
+            val source = responseBody.source()
+            source.request(Long.MAX_VALUE) // Buffer the entire body.
+            var buffer = source.buffer
+
+            var gzippedLength: Long? = null
+            if ("gzip".equals(headers["Content-Encoding"], ignoreCase = true)) {
+                gzippedLength = buffer.size
+                GzipSource(buffer.clone()).use { gzippedResponseBody ->
+                    buffer = Buffer()
+                    buffer.writeAll(gzippedResponseBody)
+                }
+            }
+
+            val contentType = responseBody.contentType()
+            val charset: Charset = contentType?.charset(StandardCharsets.UTF_8)
+                    ?: StandardCharsets.UTF_8
+
+            if (!buffer.isProbablyUtf8()) {
+                //出现Error
+                snifferLog.response.body.appendln("(binary ${buffer.size}-byte body omitted)")
+                return
+            }
+
+            if (snifferLog.response.contentLength != 0L) {
+                snifferLog.response.body.appendln(buffer.clone().readString(charset))
+            }
+
+            if (gzippedLength != null) {
+                snifferLog.response.body.appendln("(${buffer.size}-byte, $gzippedLength-gzipped-byte body)")
+            } else {
+                snifferLog.response.body.appendln("(${buffer.size}-byte body)")
+            }
+
+            //请求结束
+            snifferLog.request.requestState = RequestState.REQUEST_STATE_FINISHED
+            //通知数据更新
+            RequestCache.onSniffListener?.onNotifyDataChanged(snifferLog)
+        }
+    }
+
+    private fun setRequestData(request: Request): SnifferLog {
         val id = request.hashCode().toString()
         val url = request.url.toUrl()
         val snifferLog = SnifferLog(
@@ -29,64 +92,6 @@ class OkHttpSniffer : Interceptor {
         snifferLog.request.headers = request.headers.toMultimap()
         RequestCache.put(snifferLog.id, snifferLog)
         RequestCache.onSniffListener?.onNotifyDataChanged(snifferLog)
-        val startNs = System.nanoTime()
-        //处理响应
-        val response: Response
-
-        try {
-            response = chain.proceed(request)
-        } catch (e: Exception) {
-            //出现Error
-            snifferLog.response.body.appendln("Error:${e.message}")
-            throw e
-        }
-        val responseBody = response.body!!
-        val headers = response.headers
-        snifferLog.response.contentLength = responseBody.contentLength()
-        snifferLog.response.code = response.code
-        snifferLog.statusCode = response.code.toString()
-        snifferLog.response.message = if (response.message.isEmpty()) "" else ' ' + response.message
-        snifferLog.response.duration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
-        snifferLog.response.scheme = snifferLog.url.protocol
-        snifferLog.response.headers = response.headers.toMultimap()
-
-        val source = responseBody.source()
-        source.request(Long.MAX_VALUE) // Buffer the entire body.
-        var buffer = source.buffer
-
-        var gzippedLength: Long? = null
-        if ("gzip".equals(headers["Content-Encoding"], ignoreCase = true)) {
-            gzippedLength = buffer.size
-            GzipSource(buffer.clone()).use { gzippedResponseBody ->
-                buffer = Buffer()
-                buffer.writeAll(gzippedResponseBody)
-            }
-        }
-
-        val contentType = responseBody.contentType()
-        val charset: Charset = contentType?.charset(StandardCharsets.UTF_8)
-                ?: StandardCharsets.UTF_8
-
-        if (!buffer.isProbablyUtf8()) {
-            //出现Error
-            snifferLog.response.body.appendln("(binary ${buffer.size}-byte body omitted)")
-            return response
-        }
-
-        if (snifferLog.response.contentLength != 0L) {
-            snifferLog.response.body.appendln(buffer.clone().readString(charset))
-        }
-
-        if (gzippedLength != null) {
-            snifferLog.response.body.appendln("(${buffer.size}-byte, $gzippedLength-gzipped-byte body)")
-        } else {
-            snifferLog.response.body.appendln("(${buffer.size}-byte body)")
-        }
-
-        //请求结束
-        snifferLog.request.requestState = RequestState.REQUEST_STATE_FINISHED
-        //通知数据更新
-        RequestCache.onSniffListener?.onNotifyDataChanged(snifferLog)
-        return response
+        return snifferLog
     }
 }
